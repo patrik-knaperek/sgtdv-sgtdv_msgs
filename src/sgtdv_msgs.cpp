7f1d0169531716af679d89ba5220e48d77ecf026
Patrik Knaperek <pknaperek@gmail.com>
2022-04-27 19:03:39 +0000

    Sensor detections, simple fusion and visualization

---
+++ src/sgtdv_msgs.cpp
@@ -0,0 +1,16 @@
+#include <sgtdv_msgs/CarPose.h>
+#include <sgtdv_msgs/ColoredConeArr.h>
+#include <sgtdv_msgs/Cone.h>
+#include <sgtdv_msgs/ConeArr.h>
+#include <sgtdv_msgs/ConeWithCov.h>
+#include <sgtdv_msgs/ConeWithCovArr.h>
+#include <sgtdv_msgs/Control.h>
+#include <sgtdv_msgs/DebugState.h>
+#include <sgtdv_msgs/FusionMsg.h>
+#include <sgtdv_msgs/PathPlanningMsg.h>
+#include <sgtdv_msgs/PathTrackingMsg.h>
+#include <sgtdv_msgs/Point2D.h>
+#include <sgtdv_msgs/Point2DArr.h>
+#include <sgtdv_msgs/SLAMMsg.h>
+
+int main() { return 0; }
