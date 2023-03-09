6754e81a8ad39b6dc9fadebea4bd0e1e6f68713b
Patrik Knaperek <pknaperek@gmail.com>
2023-03-09 21:19:41 +0100

    Path planning integration and refractor (BP S. Mazur)
    - Lagrange interpolating polynomial for reactive navigation
    - RRT* algorithm for global navigation
    
    PTP trajectory planning and control on RC car
    - added posibility to start/stop vehicle with topics
    - corrected RC vehicle parameters
    - code revision
    - 'path_tracking' README update
    
    BP Path planning integration
    
    - Path planning implementation by S. Mazur
    - reactive navigation: generating center line based on linear
    interpolation of cones
    - global navigation: RRT Star algorithm
    
    'slam_sim_interface' package
    
    Point2D.msg (without header), Point2DStamped.msg (with header)
    
    'path_planning' refractoring
    
     - working with FSSIM along with 'path_tracking'
    
    RRT* Path Planning - Added angle restriction
    
    - using only one tree to find trajectory between start and end
    
    Switching from reactive to global navigation after loop closure
    
    Deleted path_planning_visualizator package; 'path_planning': cv::Vec2f -> Eigen::Vector2f
    
    Updated README

--- src/sgtdv_msgs.cpp
+++ src/sgtdv_msgs.cpp
@@ -1,16 +1,20 @@
-#include <sgtdv_msgs/CarPose.h>
-#include <sgtdv_msgs/CarVel.h>
-#include <sgtdv_msgs/ColoredConeArr.h>
+#include <sgtdv_msgs/Point2D.h>
+#include <sgtdv_msgs/Point2DArr.h>
+#include <sgtdv_msgs/Point2DStamped.h>
+#include <sgtdv_msgs/Point2DStampedArr.h>
 #include <sgtdv_msgs/Cone.h>
 #include <sgtdv_msgs/ConeArr.h>
+#include <sgtdv_msgs/ConeStamped.h>
+#include <sgtdv_msgs/ConeStampedArr.h>
 #include <sgtdv_msgs/ConeWithCov.h>
 #include <sgtdv_msgs/ConeWithCovArr.h>
+#include <sgtdv_msgs/ColoredConeArr.h>
+#include <sgtdv_msgs/CarPose.h>
+#include <sgtdv_msgs/CarVel.h>
 #include <sgtdv_msgs/Control.h>
 #include <sgtdv_msgs/DebugState.h>
 #include <sgtdv_msgs/FusionMsg.h>
 #include <sgtdv_msgs/PathPlanningMsg.h>
 #include <sgtdv_msgs/PathTrackingMsg.h>
-#include <sgtdv_msgs/Point2D.h>
-#include <sgtdv_msgs/Point2DArr.h>
 #include <sgtdv_msgs/SLAMMsg.h>
 int main() { return 0; }
